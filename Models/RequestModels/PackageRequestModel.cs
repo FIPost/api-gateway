8e87515ab020e1dd99f7e5156b5760cf6cb1c873
EGKangaroo <EGKangaroo@yahoo.com>
2021-04-15 15:09:49 +0200

    gateway fix, same request model as package service

--- Models/RequestModels/PackageRequestModel.cs
+++ Models/RequestModels/PackageRequestModel.cs
@@ -7,10 +7,9 @@ namespace api_gateway.Models.RequestModels
 {
     public class PackageRequestModel
     {
-        public string ReceiverId { get; set; }
-        public string CollectionPointId { get; set; }
-        public string TrackAndTraceId { get; set; }
         public string Sender { get; set; }
+        public string ReceiverId { get; set; }
         public string Name { get; set; }
+        public string CollectionPointId { get; set; }
     }
 }
