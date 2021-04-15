df040b107bf968df3fb5287839f47eb1502d3627
Aron Heesakkers <aron.heesakkers@via.nl>
2021-04-15 14:59:58 +0200

    Changes

--- Models/RequestModels/PackageRequestModel.cs
+++ Models/RequestModels/PackageRequestModel.cs
@@ -7,9 +7,9 @@ namespace api_gateway.Models.RequestModels
 {
     public class PackageRequestModel
     {
-        public int ReceiverId { get; set; }
-        public int CollectionPointId { get; set; }
-        public int TrackAndTraceId { get; set; }
+        public string ReceiverId { get; set; }
+        public string CollectionPointId { get; set; }
+        public string TrackAndTraceId { get; set; }
         public string Sender { get; set; }
         public string Name { get; set; }
     }
