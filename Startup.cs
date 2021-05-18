74a8853e01359a920b1ee0f09c015003353c4ae1
Aron Heesakkers <aron.heesakkers@via.nl>
2021-05-18 10:59:51 +0200

    Add kestral

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +30,9 @@ namespace api_gateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(
+            Configuration.GetSection("Kestrel"));
+
             Constants.PackageApiUrl = Configuration.GetValue<string>("PackageApiUrl");
             Constants.LocationApiUrl = Configuration.GetValue<string>("LocationApiUrl");
             Constants.PersonApiUrl = Configuration.GetValue<string>("PersonApiUrl");
